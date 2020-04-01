39d7b28d29c5d82531384d8de86576f53d27b583
Mihails B <1mihails.b@gmail.com>
2020-04-01 18:21:31 +0200

    add nickname choice path for repeated use

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {UserProvider} from './State/UserContext'
 import Unauthorized from './Components/Unauthorized';
 import {ProtectedRoute} from './Components/ProtectedRoute'
+import ChooseNickname from './Components/ChooseNickname/ChooseNickname'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/unauthorized" component={Unauthorized} />
           <ProtectedRoute path="/about" component={About}/>
           <ProtectedRoute path="/account" component={Account} />
+          <ProtectedRoute path="/chooseNickname" component={ChooseNickname}/>
       </Switch>
       </Router>
     </UserProvider>
