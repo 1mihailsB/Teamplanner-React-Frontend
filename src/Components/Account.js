1e7f0faf1a6afb3d66ace07fa51e1ac42faefcf5
Mihails B <1mihails.b@gmail.com>
2020-04-01 18:21:48 +0200

    cleanup

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,13 +1,9 @@
 import React, {useContext} from 'react'
 import {UserContext} from '../State/UserContext'
-import Cookies from 'js-cookie'
-import Unauthorized from './Unauthorized'
 
 export default function Account(){
 
-    const [user, setUser] = useContext(UserContext)
-   
-    console.log('cookie',Cookies.get('nickname'))
+    const [user] = useContext(UserContext)
        
     return(
     <h2>Welcome, {String(user)} !</h2>
