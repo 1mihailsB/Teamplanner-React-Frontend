1f7c606960956f92e2a151a45c17efa0f134e6ca
Mihails B <1mihails.b@gmail.com>
2020-05-14 14:23:42 +0200

    add dropdown to invite friends to your game plan

---
+++ src/Components/Forms/AddGameMember.js
@@ -0,0 +1,95 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faPlusCircle} from '@fortawesome/free-solid-svg-icons'
+import $ from 'jquery'
+import {properties} from "../../Properties/Properties"
+import Cookies from 'js-cookie'
+import {UserContext} from '../../State/UserContext'
+import {useHistory, useLocation} from "react-router-dom"
+
+export default function AddGameMember(props){
+
+    const [invitableFriends, setInvitableFriends] = useState([])
+    const[, setUser] = useContext(UserContext)
+    const history = useHistory();
+    const location = useLocation();
+
+    const getInvitableMembers = () => {
+        const gameId = props.gameId;
+        setUser(Cookies.get('nickname'));
+        fetch(properties.getFriendsInvitableToGame+gameId, {
+            credentials: 'include',
+            method: 'GET',
+            headers:{
+                'Accept':'application/json'
+            }
+        }).then(response => {
+            if(response.status===403){
+                setInvitableFriends('Error');
+                return "403";
+            }
+            return response.json();
+        }).then( data => {
+            if(data.toString()!=="403"){
+                setInvitableFriends(data)
+            }
+        })
+    }
+
+    const addMember = (invitedNickname) => {
+        setUser(Cookies.get('nickname'));
+        fetch(properties.inviteFriendToGame+props.gameId, {
+            credentials: 'include',
+            method: 'PUT',
+            headers: {
+                'Accept':'text/plain',
+                'Content-type':'text/plain'
+            },
+            body: invitedNickname
+        }).then(response => {//if user timed out - response status will not be 200
+            if(response.status!==200){//then update user cotnext to update all components
+                setUser(Cookies.get('nickname'))
+            }
+            return response.text()})
+        }
+
+  
+
+    $('.dropright').unbind().on('show.bs.dropdown', function () {
+        getInvitableMembers()
+    })
+    
+    useEffect(() => {
+        getInvitableMembers()
+    }, [])
+
+
+    return(
+        <div className="btn-group dropright" >
+            <button type="button" id="add-member" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" >
+                <FontAwesomeIcon size="1x" icon={faPlus} />
+            </button>
+            <div className="dropdown-menu mt-5 pl-1" id="addMemberDropdown" >
+                <h6 className="regular-text text-dark">Add friend to plan</h6>
+                <hr className="mb-0 mt-0 pb-2"/>
+                <div className="requests-container">
+                {invitableFriends.map(friend => (
+
+                    <div className="friend-request sub-bar" key={friend}>
+                        <div className="border rounded-lg border-secondary inner dropdown-inner pr-1 pl-1 pb-1 mb-1 h6" 
+                            id="dropdown-nicknames"> {friend}
+                        </div>
+
+                        <button id="accept-friendrequest" onClick={() => addMember(friend)}>
+                            <FontAwesomeIcon icon={faPlusCircle}/>
+                        </button>
+                        
+                    </div>
+
+                ))}
+                </div>
+            </div>
+        </div>
+    )
+} 
