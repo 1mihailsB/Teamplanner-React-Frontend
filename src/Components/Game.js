a80be875e1bbf00df25ca7486f1d467d99dcd5b8
Mihails B <1mihails.b@gmail.com>
2020-05-14 14:15:42 +0200

    add member elements to gameplan. remove passing initial plan text to game plan edit page

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,13 +8,15 @@ import { faEdit} from '@fortawesome/free-solid-svg-icons'
 import Status from './Forms/formUtils/Status'
 import {faTimesCircle} from '@fortawesome/free-solid-svg-icons'
 import ModalConfirmationDialog from './Utils/ModalConfirmationDialog'
+import AddGameMember from './Forms/AddGameMember'
 
-export default function Game({match}){
+export default function Game(props){
 
     const [game, setGame] = useState({})
+    const [gameMembers, setGameMembers] = useState([])
     const [, setUser] = useContext(UserContext)
     const history = useHistory()
-
+    const match = props.match
 
     useEffect(() => {
         const gameId = [match['params']['id']];
@@ -35,6 +37,7 @@ export default function Game({match}){
             }).then( data => {
                 if(data.toString()!=="403"){
                     setGame(data)
+                    setGameMembers(data.members)
                 }
             })
         }
@@ -43,11 +46,24 @@ export default function Game({match}){
     const deleteGame = (gameId) => {
         fetch(properties.deleteGameUri+gameId, {
             credentials: 'include',
-            method: 'DELETE',
+            method: 'DELETE'
         }).then(response => {
             history.push("/games")
         })
     }
+
+    const removeGameMember = (nicknameAndGameId) => {
+        console.log(nicknameAndGameId[1])
+        fetch(properties.removeGameMember+nicknameAndGameId[1], {
+            credentials: 'include',
+            method: 'DELETE',
+            body: nicknameAndGameId[0]
+        }).then(response => {
+            console.log("response")
+            history.push("/game/"+nicknameAndGameId[1])
+        })
+    }
+  
 
     return(
         <div className="col-12 mt-2 mb-4" >
@@ -56,10 +72,7 @@ export default function Game({match}){
             :
                 <React.Fragment>
                     <div className="col-12">
-                    <Link to={{
-                        pathname:"/editPlan/"+game.id, 
-                        state: {mainText: game.mainText}
-                        }} >
+                    <Link to={"/editPlan/"+game.id} >
                         <button id="edit-button">
                             <FontAwesomeIcon icon={faEdit} size="2x"/>
                         </button>
@@ -70,12 +83,36 @@ export default function Game({match}){
                         <div className="card-body mb-0 p-0">
                             <ModalConfirmationDialog actionPrefix="game" functionArgument={game.id} 
                             functionToExecute={deleteGame} warningText="Deleting: " warningArgument={game.title}/>
-                            <button id="delete-game" data-toggle="modal" data-target={"#modalPopupId"+"game"+game.id}>
+                            <button id="delete-game" data-toggle="modal" data-target={"#modalPopupIdgame"+game.id}>
                                 <FontAwesomeIcon icon={faTimesCircle}/>
                             </button>
                             <h5 className="card-title h5 pl-3 pr-4">{game.title}</h5>
                         </div>
+
                         <hr className="mb-0 mt-0"/>
+                        
+                        <AddGameMember gameId={[match['params']['id']]} />
+                        <h5 className="pl-4 pr-1">Members:</h5>
+
+                        <div className="plan-members pl-2 mt-1 ml-1">
+                            
+                            {gameMembers.map(member => (
+                                <div className="game-member border rounded-lg border-secondary mr-1 pr-1 pl-1 pb-1 mb-1 h6"
+                                key={"member"+member}>
+                                    <ModalConfirmationDialog actionPrefix="gameMember" functionArgument={[member,game.id]} 
+                                    functionToExecute={removeGameMember} warningText="Removing: " warningArgument={member}/>
+                                    <div>{member}</div>
+                                    <button className="pl-1" id="remove-gamemember" data-toggle="modal" 
+                                        data-target={"#modalPopupIdgameMember"+member+game.id}>
+                                        <FontAwesomeIcon icon={faTimesCircle}/>
+                                    </button>
+                                </div>
+                            ))}
+                            
+
+                        </div>
+                        
+                        <hr className="mb-0 mt-1 mb-1" id="main-text-dividor"/>
                         <pre className="ml-3 mainText">{game.mainText}</pre>
                         <hr className="mb-0 mt-0"/>
                         <h6 className="ml-3">By: {game.authorNickname}</h6>
