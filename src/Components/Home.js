fa4b54f45b0ffd98fb214daa70dbf392515ca384
Mihails B <1mihails.b@gmail.com>
2020-05-19 14:46:15 +0200

    edit home page and change back end url property

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,20 @@ import React from 'react'
 export default function Home(){
 
     return(
-        <div className="regular-text">
+        
+        <div className="welcome">
             <h1 id="welcome">Welcome !</h1>
+            <div className="welcome-description">
+                <p>Log in and choose a nickname</p>
+                <p>Create a plan for any needs you want</p>
+                <ul>
+                    <li>Meeting</li>
+                    <li>Event</li>
+                    <li>Game session with your friends</li>
+                    <li>Just a reminder or a note for yourself</li>
+                </ul>
+                <p>Add your friends by the nickname they have chosen and invite them to your plans</p>
+            </div>
         </div>
     )
 }
--- src/Properties/Properties.js
+++ src/Properties/Properties.js
@@ -1,4 +1,4 @@
-const backEndAddress = "https://springboot-teamplanner.herokuapp.com/"
+const backEndAddress = "http://34.89.241.79/"
 
 export const properties = {
     clientId: "524272934250-0eeakh761npvpo8th9s1lepqobgebjj5.apps.googleusercontent.com",
