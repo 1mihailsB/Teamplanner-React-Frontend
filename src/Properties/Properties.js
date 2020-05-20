d8b66ababc4c53f6d8fcff6877613336231d21ae
Mihails B <1mihails.b@gmail.com>
2020-05-20 17:44:00 +0200

    change back end address

--- src/Properties/Properties.js
+++ src/Properties/Properties.js
@@ -1,4 +1,4 @@
-const backEndAddress = "https://34.89.241.79/"
+const backEndAddress = "https://teamplanner-springboot-rest.ey.r.appspot.com/"
 
 export const properties = {
     clientId: "524272934250-0eeakh761npvpo8th9s1lepqobgebjj5.apps.googleusercontent.com",
